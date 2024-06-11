baeb60f7fbcba233fc25b4c2d52af94c991bef72
cronan03 <ronancoutinho03@gmail.com>
2024-06-11 16:16:34 +0530

    test_tpl

--- tests/test_tpl.py
+++ tests/test_tpl.py
@@ -1,9 +1,5 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
-<<<<<<< HEAD
-=======
-
->>>>>>> ac7c1e03bb773db0a2a25814358ed4ebb690bf88
 def test_tpl():
     assert True
