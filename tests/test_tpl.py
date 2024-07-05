1234f3dcb3e1d59e0d3101a8a1d738cf327cbb21
cronan03 <ronancoutinho03@gmail.com>
2024-07-05 18:32:21 +0530

    build tests

--- tests/test_tpl.py
+++ tests/test_tpl.py
@@ -3,12 +3,8 @@
 
 
 from copier import run_copy
-
 from subprocess import run
-
-
-
-
+import os
 
 def test_tpl():
     assert True
@@ -29,9 +25,52 @@ def helper(dest):
             ,dest], cwd=dest,
               check=True)
 
-def test_dummy(tmp_path):
+def test_files_build(tmp_path):
      extension_folder  = tmp_path / "ext"
      extension_folder.mkdir()
      helper(str(extension_folder))
-# Or from a Git URL.
+     
+     run(["jlpm" ,"install"], cwd=extension_folder, check=True)
+     run(["jlpm" ,"run","build:prod"], cwd=extension_folder,check=True)
+     
+     run(["jupyter-builder", "build", str(extension_folder)], cwd=extension_folder,check=True)
 
+     folder_path = extension_folder / "myextension/labextension"
+
+     expected_files = ["static/style.js", "package.json"]
+
+     for filename in expected_files:
+       filepath = os.path.join(folder_path, filename)
+       assert os.path.exists(filepath), f"File {filename} does not exist in {folder_path}!"
+  
+def test_files_build_development(tmp_path):
+     extension_folder  = tmp_path / "ext"
+     extension_folder.mkdir()
+     helper(str(extension_folder))
+     
+     run(["jlpm" ,"install"], cwd=extension_folder, check=True)
+     run(["jlpm" ,"run","build:prod"], cwd=extension_folder,check=True)
+     
+     run(["jupyter-builder", "build","--development","true", str(extension_folder)], cwd=extension_folder,check=True)
+     
+     folder_path = extension_folder / "myextension/labextension"
+
+     expected_files = ["static/style.js", "package.json", "build_log.json"]
+
+     for filename in expected_files:
+       filepath = os.path.join(folder_path, filename)
+       assert os.path.exists(filepath), f"File {filename} does not exist in {folder_path}!"
+
+
+# def test_files_exist():
+#   """
+#   This test checks if a.py and b.py exist in a specific folder.
+#   """
+
+#   folder_path = r"D:/GSOC/JupyterBuild/jupyterlab-builder/tests/dummy/dummy1/labextension/static"
+
+#   expected_files = ["style.js", "remoteEntry.cf9febd8c72130231294.js"]
+
+#   for filename in expected_files:
+#     filepath = os.path.join(folder_path, filename)
+#     assert os.path.exists(filepath), f"File {filename} does not exist in {folder_path}!"
