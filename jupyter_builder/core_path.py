ea937f2057c47900d723aef29d61fc3a00d3d8bc
darshan <pranishpoudel10@gmail.com>
2026-02-15 14:26:58 +0545

    use-github-to-get-core-meta

--- jupyter_builder/core_path.py
+++ jupyter_builder/core_path.py
@@ -1,10 +1,32 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
+import tempfile
 from pathlib import Path
 
+import requests
 
-def default_core_path() -> str:
-    import jupyterlab
 
-    return str(Path(jupyterlab.__file__).parent / "staging")
+def create_temp_core_path(core_version=None) -> str:
+    """
+    Download JupyterLab staging/package.json into a temporary directory
+    and return the path to that staging directory.
+    """
+    version = core_version or "main"
+    url = (
+        "https://raw.githubusercontent.com/"
+        f"jupyterlab/jupyterlab/{version}/"
+        "jupyterlab/staging/package.json"
+    )
+
+    # Create persistent temporary directory
+    tmpdir = tempfile.mkdtemp(prefix="jupyterlab-staging-")
+    staging_path = Path(tmpdir)
+
+    # Download package.json
+    response = requests.get(url, timeout=10)
+    response.raise_for_status()
+
+    (staging_path / "package.json").write_bytes(response.content)
+
+    return str(staging_path)
--- jupyter_builder/extension_commands/build.py
+++ jupyter_builder/extension_commands/build.py
@@ -6,7 +6,6 @@ import os
 from traitlets import Bool, Unicode
 
 from jupyter_builder.base_extension_app import BaseExtensionApp
-from jupyter_builder.core_path import default_core_path
 from jupyter_builder.federated_extensions import build_labextension
 
 HERE = os.path.dirname(os.path.abspath(__file__))
@@ -22,9 +21,15 @@ class BuildLabExtensionApp(BaseExtensionApp):
     source_map = Bool(False, config=True, help="Generate source maps")
 
     core_path = Unicode(
-        default_core_path(),
+        "",
         config=True,
         help="Directory containing core application package.json file",
+    )
+
+    core_version = Unicode(
+        "main",
+        config=True,
+        help="Version of JupyterLab core to use when building (ignored if core-path is set)",
     )
 
     aliases = {  # noqa: RUF012
@@ -32,6 +37,7 @@ class BuildLabExtensionApp(BaseExtensionApp):
         "development": "BuildLabExtensionApp.development",
         "source-map": "BuildLabExtensionApp.source_map",
         "core-path": "BuildLabExtensionApp.core_path",
+        "core-version": "BuildLabExtensionApp.core_version",
     }
 
     def run_task(self):
@@ -43,6 +49,7 @@ class BuildLabExtensionApp(BaseExtensionApp):
             static_url=self.static_url or None,
             source_map=self.source_map,
             core_path=self.core_path or None,
+            core_version=self.core_version or None,
         )
 
 
