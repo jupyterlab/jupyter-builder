19f6bd30b85024565109706e7939eb6a80213c4d
Darshan Poudel <pranishpoudel10@gmail.com>
2026-03-23 12:23:24 +0545

    Clean and rename to `BuilderApp` (#55)
    * clean-labextension
    
    * rename-to-BuilderApp
    
    * remove-(developer)
    
    * include-_version-file-in-wheel

--- jupyter_builder/main.py
+++ jupyter_builder/main.py
@@ -1,9 +1,6 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
-# # Copyright (c) Jupyter Development Team.
-# # Distributed under the terms of the Modified BSD License.
-
 import sys
 
 from jupyter_core.application import JupyterApp
@@ -12,30 +9,27 @@ from jupyter_builder.extension_commands.build import BuildLabExtensionApp
 from jupyter_builder.extension_commands.develop import DevelopLabExtensionApp
 from jupyter_builder.extension_commands.watch import WatchLabExtensionApp
 
+from ._version import __version__
 
-class LabExtensionApp(JupyterApp):
-    """Base jupyter labextension command entry point"""
+_EXAMPLES = """
+jupyter-builder build                       # build a prebuilt labextension
+jupyter-builder develop                     # develop a prebuilt labextension
+jupyter-builder watch                       # watch a prebuilt labextension
+"""
 
-    name = "jupyter labextension"
-    # version = VERSION
-    description = "Work with JupyterLab extensions"
-    # examples = _EXAMPLES
+
+class BuilderApp(JupyterApp):
+    """Base jupyter-builder command entry point"""
+
+    name = "jupyter builder"
+    version = __version__
+    description = "Build JupyterLab extensions"
+    examples = _EXAMPLES
 
     subcommands = {
-        # "install": (InstallLabExtensionApp, "Install labextension(s)"),
-        # "update": (UpdateLabExtensionApp, "Update labextension(s)"),
-        # "uninstall": (UninstallLabExtensionApp, "Uninstall labextension(s)"),
-        # "list": (ListLabExtensionsApp, "List labextensions"),
-        # "link": (LinkLabExtensionApp, "Link labextension(s)"),
-        # "unlink": (UnlinkLabExtensionApp, "Unlink labextension(s)"),
-        # "enable": (EnableLabExtensionsApp, "Enable labextension(s)"),
-        # "disable": (DisableLabExtensionsApp, "Disable labextension(s)"),
-        # "lock": (LockLabExtensionsApp, "Lock labextension(s)"),
-        # "unlock": (UnlockLabExtensionsApp, "Unlock labextension(s)"),
-        # "check": (CheckLabExtensionsApp, "Check labextension(s)"),
-        "develop": (DevelopLabExtensionApp, "(developer) Develop labextension(s)"),
-        "build": (BuildLabExtensionApp, "(developer) Build labextension"),
-        "watch": (WatchLabExtensionApp, "(developer) Watch labextension"),
+        "develop": (DevelopLabExtensionApp, "Develop labextension(s)"),
+        "build": (BuildLabExtensionApp, "Build labextension"),
+        "watch": (WatchLabExtensionApp, "Watch labextension"),
     }
 
     def start(self):
@@ -48,7 +42,7 @@ class LabExtensionApp(JupyterApp):
         self.exit(f"Please supply at least one subcommand: {subcmds}")
 
 
-main = LabExtensionApp.launch_instance
+main = BuilderApp.launch_instance
 
 if __name__ == "__main__":
     sys.exit(main())
