4c58ba6f78377f5a1dee2ab33acb1fd63bb2463b
darshan <pranishpoudel10@gmail.com>
2026-05-01 16:05:17 +0545

    revert-changes

--- src/build-labextension.ts
+++ src/build-labextension.ts
@@ -58,7 +58,6 @@ commander
   )
   .option('--watch')
   .action(async (options, command) => {
-    throw new Error('Test');
     const mode = options.development ? 'development' : 'production';
     const corePackageFile = path.resolve(options.corePackageFile);
     const packagePath = path.resolve(command.args[0]);
