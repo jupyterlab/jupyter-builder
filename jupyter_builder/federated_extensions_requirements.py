18a655fd52720f0589bd5feb83848d4a3498138d
darshan <pranishpoudel10@gmail.com>
2026-05-05 21:38:29 +0545

    lint-python

--- jupyter_builder/federated_extensions_requirements.py
+++ jupyter_builder/federated_extensions_requirements.py
@@ -8,20 +8,8 @@ import json
 import os.path as osp
 from glob import iglob
 from itertools import chain
-
-# from logging import Logger
 from os.path import join as pjoin
 from typing import Any
-
-# import json5  # type:ignore[import-untyped]
-# from jupyter_core.paths import SYSTEM_CONFIG_PATH, jupyter_config_dir, jupyter_path
-# from jupyter_server.services.config.manager import ConfigManager, recursive_update
-# from jupyter_server.utils import url_path_join as ujoin
-# from traitlets import Bool, HasTraits, List, Unicode, default
-
-# -----------------------------------------------------------------------------
-# Module globals
-# -----------------------------------------------------------------------------
 
 DEFAULT_TEMPLATE_PATH = osp.join(osp.dirname(__file__), "templates")
 
