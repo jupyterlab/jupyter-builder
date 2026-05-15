3b341f16b4ad8f8349765ba327359f3f563b4ec8
darshan <pranishpoudel10@gmail.com>
2026-05-15 15:42:28 +0545

    lint

--- scripts/bump_version.py
+++ scripts/bump_version.py
@@ -1,7 +1,7 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
-"""Script to bump the version of jupyter-builder."""  # noqa: INP001
+"""Script to bump the version of jupyter-builder."""
 
 import json
 from pathlib import Path
