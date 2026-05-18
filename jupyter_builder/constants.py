ab411860dc6bf27d5471901687cb1bbe60d7414e
darshan <pranishpoudel10@gmail.com>
2026-05-18 17:35:32 +0545

    fix-header

--- jupyter_builder/constants.py
+++ jupyter_builder/constants.py
@@ -1,6 +1,3 @@
-# Copyright (c) Jupyter Development Team.
-# Distributed under the terms of the Modified BSD License.
-
 """URL constants for jupyter-builder network fetches, overridable via environment variables."""
 
 # Copyright (c) Jupyter Development Team.
